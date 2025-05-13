914c0bacf5407a49a08e918bedb09032067bb090
AnhNN23 <anhnn@foxcode.info>
2025-05-13 14:56:47 +0700

    Fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    //Remove basePath when you are trying to deploy other than github page
-    basePath: "/nextjs_face_object_detection",
     images: {
         unoptimized: true,
     },
